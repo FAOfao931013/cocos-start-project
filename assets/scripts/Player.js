69f7ab46b23f1734201f1dd951e435a190ef81e6
fuhao <465476662@qq.com>
2017-11-08 16:49:21 +0800

    优化代码

--- assets/scripts/Player.js
+++ assets/scripts/Player.js
@@ -71,19 +71,19 @@ cc.Class({
     },
 
     update: function (dt) {
-        // 根据当前加速度方向每帧更新速度
+        //根据当前加速度方向每帧更新速度
         if (this.accLeft) {
             this.xSpeed -= this.accel * dt;
         } else if (this.accRight) {
             this.xSpeed += this.accel * dt;
         }
-        // 限制主角的速度不能超过最大值
+        //限制主角的速度不能超过最大值
         if (Math.abs(this.xSpeed) > this.maxMoveSpeed) {
             // if speed reach limit, use max speed with current direction
             this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
         }
 
-        // 根据当前速度更新主角的位置
+        //根据当前速度更新主角的位置
         this.node.x += this.xSpeed * dt;
 
         //场景循环跳跃
@@ -96,7 +96,6 @@ cc.Class({
         }
     },
 
-    // use this for initialization
     onLoad: function() {
         // 初始化跳跃动作
         this.jumpAction = this.setJumpAction();
@@ -112,7 +111,6 @@ cc.Class({
         this.setInputControl();
     },
 
-    // called every frame, uncomment this function to activate update callback
     // update: function (dt) {
 
     // },
--- assets/scripts/Star.js
+++ assets/scripts/Star.js
@@ -2,33 +2,31 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // 星星和主角之间的距离小于这个数值时，就会完成收集
+        //星星和主角之间的距离小于这个数值时，就会完成收集
         pickRadius: 0
     },
 
     getPlayerDistance: function () {
-        // 根据 player 节点位置判断距离
+        //根据 player 节点位置判断距离
         var playerPos = this.game.player.getPosition();
-        // 根据两点位置计算两点之间距离
+        //根据两点位置计算两点之间距离
         var dist = cc.pDistance(this.node.position, playerPos);
         return dist;
     },
 
     onPicked: function () {
-        // 当星星被收集时，调用 Game 脚本中的接口，生成一个新的星星
+        //当星星被收集时，调用 Game 脚本中的接口，生成一个新的星星
         this.game.spawnNewStar();
-        // 得分
+        //得分
         this.game.gainScore();
-        // 然后销毁当前星星节点
+        //然后销毁当前星星节点
         this.node.destroy();
     },
 
-    // use this for initialization
     onLoad: function () {
 
     },
 
-    // called every frame, uncomment this function to activate update callback
     update: function (dt) {
         if (this.getPlayerDistance() < this.pickRadius) {
             // 调用收集行为
@@ -36,7 +34,7 @@ cc.Class({
             return;
         }
 
-        // 根据 Game 脚本中的计时器更新星星的透明度
+        //根据 Game 脚本中的计时器更新星星的透明度
         var opacityRatio = 1 - this.game.timer / this.game.starDuration;
         var minOpacity = 50;
         this.node.opacity = minOpacity + Math.floor(opacityRatio * (255 - minOpacity));
